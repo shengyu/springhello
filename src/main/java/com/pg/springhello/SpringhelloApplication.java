f5ff2dadde0f20b4d000f1360f39f1f60d3c920f
Shengyu Liu <usungu@gmail.com>
2021-08-11 23:35:15 +0800

    Add hello controller

---
+++ src/main/java/com/pg/springhello/SpringhelloApplication.java
@@ -0,0 +1,13 @@
+package com.pg.springhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pg/springhello/SpringhelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.springhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringhelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
